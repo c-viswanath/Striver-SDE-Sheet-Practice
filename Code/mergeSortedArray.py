be6bfc5ecc4ebf22367f09797d4d0c346619b775
C. Vishwanath <cvishwanath1417@gmail.com>
2024-05-21 01:58:59 +0530

    Update mergeSortedArray.py

--- Code/mergeSortedArray.py
+++ Code/mergeSortedArray.py
@@ -2,4 +2,4 @@ class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         for i in range(m,len(nums1)):
             nums1[i]=nums2[i-m]
-        nums1.sort
+        nums1.sort()
